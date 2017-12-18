80cd2fb304c4ce4d2ff2a7fbbeb9ba8abe8aed85
Noah McDougall <noahmcdougall@gmail.com>
2017-12-17 20:26:34 -0600

    Trying to get the paths correct

--- wsgi/scripts/application.py
+++ wsgi/scripts/application.py
@@ -12,7 +12,7 @@ import os.path
 import io
 
 ## Sessions enabled ##
-wsgi_dir = '/home/noahmcdougall/GeoStriker/wsgi/static'
+wsgi_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)))
 conf = {
          '/': {
              'tools.sessions.on': True
@@ -25,7 +25,7 @@ conf = {
 
 
 ## Setting up jinja2's web template stuff ##
-env = jinja2.Environment(loader=jinja2.FileSystemLoader(wsgi_dir))
+env = jinja2.Environment(loader=jinja2.FileSystemLoader('/home/noahmcdougall/GeoStriker/wsgi/static'))
 
 if cherrypy.__version__.startswith('3.') and cherrypy.engine.state == 0:
     cherrypy.engine.start(blocking=False)
